93d4cf241a1ef96c0f9b84417c8fd40fb02fc377
Atnagulova <uliatnagulova@mail.ru>
2019-05-19 19:00:49 +0500

    Вроде работает, но нужно разобраться с циклом

---
+++ Task-4/Task-4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double e;//точность
+            double S=0;//сумма
+            e = WriteDouble("Введите точность: ");
+
+            for (int i = 1; i<Int32.MaxValue; i++)
+            {
+                S = S + (Math.Pow(-1, i) / Enumerable.Range(1, i).Aggregate((p, x) => p * x));
+            if (Math.Abs(S) < e)
+                {
+                    Console.WriteLine("Сумма выражения с заданной точностью {0} равна {1}", e, S);
+                    break;
+                }                
+            }
+            Console.ReadKey();
+        }
+        public static double WriteDouble(string t)
+        {
+            bool ok; double a;
+            do
+            {
+                Console.WriteLine(t);
+                string buf = Console.ReadLine();
+                ok = double.TryParse(buf, out a);
+                if ((!ok) || (a<0))
+                    Console.WriteLine("Ошибка, введите положительное число");
+            } while ((!ok) || (a < 0));
+            return a;
+        }
+    }
+}
